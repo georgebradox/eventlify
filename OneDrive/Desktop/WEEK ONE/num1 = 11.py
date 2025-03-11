00cdc046b73008ea057dc0fa4a520c9c50afcaa7
DEVLYN <devlynruth@gmail.com>
2025-03-11 22:12:11 +0300

    Initial commit

---
+++ OneDrive/Desktop/WEEK ONE/num1 = 11.py
@@ -0,0 +1,14 @@
+num1 = 11
+num2 = 9
+operation = input("Enter +, -, *, /:")
+
+if operation =="+":
+    print("11+9=",11+9)
+elif operation =="-":
+    print("11-9=",11-9)
+elif operation =="*":
+    print("11*9",11*9)
+elif operation =="/":
+    print("11/9",11/9)
+else:
+    print("Invalid operation")
